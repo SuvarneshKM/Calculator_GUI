97fc425bfc166ceae73a7c2cd37c31c9f649f010
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2020-10-30 20:41:32 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,140 +1,104 @@
 from tkinter import *
-import parser
-import math
+from tkinter import messagebox
 
-# Parser help us to solve mathematical operation
+calculator = Tk()
+calculator.title("CALCULATOR")
+calculator.resizable(0, 1)#remove or change this in order to get different screen sizes
 
-root = Tk()
-root.title('CALCULATOR')
-root.geometry('670x450')
-root.configure(bg='Blue')
+class Application(Frame):
+	def __init__(self, master, *args, **kwargs):
+		Frame.__init__(self, master, *args, **kwargs)
+		self.createWidgets()
 
-# get the user input and place it in the text field
-i = 0
+	def replaceText(self, text):
+		self.display.delete(0, END)
+		self.display.insert(0, text)
+
+	def appendToDisplay(self, text):
+		self.entryText = self.display.get()
+		self.textLength = len(self.entryText)
+
+		if self.entryText == "0":
+			self.replaceText(text)
+		else:
+			self.display.insert(self.textLength, text)
+
+	def calculateExpression(self):#python's calculate function 
+		self.expression = self.display.get()
+		self.expression = self.expression.replace("%", "/ 100")
+
+		try:
+			self.result = eval(self.expression)
+			self.replaceText(self.result)
+		except:
+			messagebox.showinfo("ERROR", "Invalid input", icon="warning", parent=calculator)
+
+	def clearText(self):#clears imput on pressing C on Calculator
+		self.replaceText("0")
+
+	def createWidgets(self):
+		self.display = Entry(self, font=("Helvetica", 16), borderwidth=0, relief=RAISED, justify=RIGHT)
+		self.display.insert(0, "0")
+		self.display.grid(row=0, column=0, columnspan=5)
+
+#This is the First Row
+		self.sevenButton = Button(self, font=("Helvetica", 11), text="7", borderwidth=0, command=lambda: self.appendToDisplay("7"))
+		self.sevenButton.grid(row=1, column=0, sticky="NWNESWSE")
+
+		self.eightButton = Button(self, font=("Helvetica", 11), text="8", borderwidth=0, command=lambda: self.appendToDisplay("8"))
+		self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
+
+		self.nineButton = Button(self, font=("Helvetica", 11), text="9", borderwidth=0, command=lambda: self.appendToDisplay("9"))
+		self.nineButton.grid(row=1, column=2, sticky="NWNESWSE")
+
+		self.timesButton = Button(self, font=("Helvetica", 11), text="*", borderwidth=0, command=lambda: self.appendToDisplay("*"))
+		self.timesButton.grid(row=1, column=3, sticky="NWNESWSE")
+
+		self.clearButton = Button(self, font=("Helvetica", 11), text="C", borderwidth=0, command=lambda: self.clearText())
+		self.clearButton.grid(row=1, column=4, sticky="NWNESWSE")
+
+#This is the Second Row
+		self.fourButton = Button(self, font=("Helvetica", 11), text="4", borderwidth=0, command=lambda: self.appendToDisplay("4"))
+		self.fourButton.grid(row=2, column=0, sticky="NWNESWSE")
+
+		self.fiveButton = Button(self, font=("Helvetica", 11), text="5", borderwidth=0, command=lambda: self.appendToDisplay("5"))
+		self.fiveButton.grid(row=2, column=1, sticky="NWNESWSE")
+
+		self.sixButton = Button(self, font=("Helvetica", 11), text="6", borderwidth=0, command=lambda: self.appendToDisplay("6"))
+		self.sixButton.grid(row=2, column=2, sticky="NWNESWSE")
+
+		self.divideButton = Button(self, font=("Helvetica", 11), text="/", borderwidth=0, command=lambda: self.appendToDisplay("/"))
+		self.divideButton.grid(row=2, column=3, sticky="NWNESWSE")
+
+		self.percentageButton = Button(self, font=("Helvetica", 11), text="%", borderwidth=0, command=lambda: self.appendToDisplay("%"))
+		self.percentageButton.grid(row=2, column=4, sticky="NWNESWSE")
+
+#This is the Third Row
+		self.oneButton = Button(self, font=("Helvetica", 11), text="1", borderwidth=0, command=lambda: self.appendToDisplay("1"))
+		self.oneButton.grid(row=3, column=0, sticky="NWNESWSE")
+
+		self.twoButton = Button(self, font=("Helvetica", 11), text="2", borderwidth=0, command=lambda: self.appendToDisplay("2"))
+		self.twoButton.grid(row=3, column=1, sticky="NWNESWSE")
+
+		self.threeButton = Button(self, font=("Helvetica", 11), text="3", borderwidth=0, command=lambda: self.appendToDisplay("3"))
+		self.threeButton.grid(row=3, column=2, sticky="NWNESWSE")
+
+		self.minusButton = Button(self, font=("Helvetica", 11), text="-", borderwidth=0, command=lambda: self.appendToDisplay("-"))
+		self.minusButton.grid(row=3, column=3, sticky="NWNESWSE")
+
+		self.equalsButton = Button(self, font=("Helvetica", 11), text="=", borderwidth=0, command=lambda: self.calculateExpression())
+		self.equalsButton.grid(row=3, column=4, sticky="NWNESWSE", rowspan=2)
+
+#This is the Fourth Row
+		self.zeroButton = Button(self, font=("Helvetica", 11), text="0", borderwidth=0, command=lambda: self.appendToDisplay("0"))
+		self.zeroButton.grid(row=4, column=0, columnspan=2, sticky="NWNESWSE")
+
+		self.dotButton = Button(self, font=("Helvetica", 11), text=".", borderwidth=0, command=lambda: self.appendToDisplay("."))
+		self.dotButton.grid(row=4, column=2, sticky="NWNESWSE")
+
+		self.plusButton = Button(self, font=("Helvetica", 11), text="+", borderwidth=0, command=lambda: self.appendToDisplay("+"))
+		self.plusButton.grid(row=4, column=3, sticky="NWNESWSE")
 
 
-def get_variables(num):
-    global i
-    display.insert(i, num)
-    i += 1
-
-
-def calculate():
-    entire_string = display.get()
-    try:
-        a = parser.expr(entire_string).compile()
-        result = eval(a)
-        clear_all()
-        display.insert(0, result)
-
-    except EXCEPTION:
-        clear_all()
-        display.insert(0, "ERROR")
-
-
-def factorial():
-    n = int(display.get())
-    fact = math.factorial(n)
-    clear_all()
-    display.insert(0, fact)
-
-
-# adding functionality
-def get_operation(operator):
-    global i
-    length = len(operator)
-    display.insert(i, operator)
-    i += length
-
-
-# deleting entire elements on the screen
-def clear_all():
-    display.delete(0, END)
-
-
-# deleting single element on the screen
-def undo():
-    entire_string = display.get()
-    if len(entire_string):
-        new_string = entire_string[:-1]
-        clear_all()
-        display.insert(0, new_string)
-    else:
-        clear_all()
-        display.insert(0, "ERROR")
-
-
-# adding the input field
-
-display = Entry(root, font=('Helvetica', '40'))
-display.grid(row=1, columnspan=6, padx=45, pady=35, sticky=W + E)
-
-# adding buttons to calculator
-Button(root, text="1", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(1)).grid(row=2, column=0)
-Button(root, text="2", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(2)).grid(row=2, column=1)
-Button(root, text="3", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(3)).grid(row=2, column=2)
-
-Button(root, text="4", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(4)).grid(row=3, column=0)
-Button(root, text="5", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(5)).grid(row=3, column=1)
-Button(root, text="6", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(6)).grid(row=3, column=2)
-
-Button(root, text="7", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(7)).grid(row=4, column=0)
-Button(root, text="8", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(8)).grid(row=4, column=1)
-Button(root, text="9", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(9)).grid(row=4, column=2)
-
-# adding other buttons to the calculator
-Button(root, text="AC", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: clear_all()).grid(row=5, column=0)
-Button(root, text="0", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(0)).grid(row=5, column=1)
-Button(root, text="=", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: calculate()).grid(row=5, column=2)
-
-Button(root, text="+", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("+")).grid(row=2,
-                                                column=3)
-Button(root, text="-", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("-")).grid(row=3,
-                                                column=3)
-Button(root, text="*", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("*")).grid(row=4,
-                                                column=3)
-Button(root, text="/", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("/")).grid(row=5,
-                                                column=3)
-
-# adding new operations
-Button(root, text="pi", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("*3.14")).grid(row=2,
-                                                    column=4)
-Button(root, text="%", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("%")).grid(row=3,
-                                                column=4)
-Button(root, text="(", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("(")).grid(row=4,
-                                                column=4)
-Button(root, text="exp", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("**")).grid(row=5,
-                                                 column=4)
-
-Button(root, text=">-", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: undo()).grid(row=2, column=5)
-Button(root, text="!", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: factorial()).grid(row=3, column=5)
-Button(root, text=")", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation(")")).grid(row=4,
-                                                column=5)
-Button(root, text="^2", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("**2")).grid(row=5,
-                                                  column=5)
-
-root.mainloop()
+app = Application(calculator).grid()		
+calculator.mainloop()
